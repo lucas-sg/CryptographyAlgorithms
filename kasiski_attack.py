0437366a934c21ffccb5d602b23155e2ea48185c
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2020-03-15 23:44:33 -0300

    Fixed: text not being multiple of key length + style corrections

--- kasiski_attack.py
+++ kasiski_attack.py
@@ -11,54 +11,72 @@ SPA_ALPH = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
 def kasiski_decipher(s):
     for keys in calc_keys(s):
-        msg = [shift_char(s[i], -keys[i%len(keys)]) for i in range(len(s))]
+        msg = [shift_char(s[i], -keys[i % len(keys)]) for i in range(len(s))]
         print('\n' + str(keys) + ': ' + ''.join(msg))
 
 
 def calc_keys(s):
-    possible_keys = []
+    keys = []
     for key_length in calc_key_lengths(s):
         k_cols = calc_k_cols_for_key_length(s, key_length)
-        possible_keys.append([break_shift_cipher(col) for col in k_cols])
-    return possible_keys
+        possible_keys = [break_shift_cipher(col.rstrip()) for col in k_cols]
+        keys.append(possible_keys)
+    return keys
 
 
 def calc_k_cols_for_key_length(s, key_length):
-    groupings = [s[i:i+key_length] for i in range(0, len(s), key_length)]
-    k_cols = [[row[i] for row in groupings] for i in range(key_length)]
-    return ["".join(s) for s in k_cols]
+    n_grams = calc_n_grams(s, key_length)
+    k_cols = [[row[i] for row in n_grams] for i in range(key_length)]
+    return [''.join(s) for s in k_cols]
+
+
+def calc_n_grams(s, n):
+    rmdr = len(s) % n
+    mult = len(s)-rmdr    # Multiple of n
+    n_grams = [s[i:i+n] for i in range(0, mult, n)]
+    add_padding(s, rmdr, n_grams, n)
+    return n_grams
+
+
+def add_padding(s, rmdr, n_grams, n):
+    tail = s[:rmdr]
+    if tail:
+        n_grams.append((tail).ljust(len(tail)+n-rmdr))
+    return
 
 
 def calc_key_lengths(s):
     lengths = []
-    for seq_len in np.arange(3, len(s)):
-        seq_pairs = [seq_offset_pair(s, i, seq_len) for i in np.arange(0, len(s)-seq_len)]
-        all_offsets = flatten([pair[1] for pair in seq_pairs if pair[1]])
+    for n in np.arange(3, int(len(s)/2)):
+        all_offsets = offsets_for_n_grams(s, n)
         if all_offsets:
-            lengths.append(mcd(all_offsets))       
+            mcd = reduce(gcd, all_offsets)
+            if mcd > 1:
+                lengths.append(mcd)
     return lengths
-        
-
-def seq_offset_pair(s, i, seq_len):
-    sequence = s[i:i+seq_len]
-    offsets = offsets_between_repetitions(s, sequence)
-    return [sequence, offsets]
 
 
-# Example:   aasdfkjXXXksajkljXXXooieruXXXppperXXX  should return 3 ints for XXX
+def offsets_for_n_grams(s, n):
+    seen_n_grams = []
+    all_offsets = []
+    for i in np.arange(0, len(s)-n):
+        n_gram = s[i:i+n]
+        if n_gram not in seen_n_grams:
+            all_offsets.append(offsets_between_repetitions(s, n_gram))
+        seen_n_grams.append(n_gram)
+    return flatten([offsets for offsets in all_offsets if offsets])
+
+
+# Example:  aasdfkjXXXksajkljXXXooieruXXXppperXXX  should return 3 ints for XXX
 # And something such as:   wiulnsXXXuioop   should return an empty list
 def offsets_between_repetitions(s, subs):
     if s.count(subs) <= 1:
-        return [] 
+        return []
     inter_strings = (s.split(subs))[1:]
     offsets = [len(inter_s) + len(subs) for inter_s in inter_strings]
     if not s.endswith(subs):
         offsets.pop()
     return offsets
-
-
-def mcd(list):
-    return reduce(gcd, list)
 
 
 def flatten(list):
@@ -67,7 +85,4 @@ def flatten(list):
 
 C1 = "PBVRQVICADSKAÑSDETSJPSIEDBGGMPSLRPWRÑPWYEDSDEÑDRDPCRCPQMNPWKUBZVSFNVRDMTIPWUEQVVCBOVNUEDIFQLONMWNUVRSEIKAZYEACEYEDSETFPHLBHGUÑESOMEHLBXVAEEPUÑELISEVEFWHUNMCLPQPMBRRNBPVIÑMTIBVVEÑIDANSJAMTJOKMDODSELPWIUFOZMQMVNFOHASESRJWRSFQCOTWVMBJGRPWVSUEXINQRSJEUEMGGRBDGNNILAGSJIDSVSUEEINTGRUEETFGGMPORDFOGTSSTOSEQOÑTGRRYVLPWJIFWXOTGGRPQRRJSKETXRNBLZETGGNEMUOTXJATORVJHRSFHVNUEJIBCHASEHEUEUOTIEFFGYATGGMPIKTBWUEÑENIEEU"
 C2 = "JGAZNWINHYLZDYVBBJLCQHTNKUDQXMOXJNOZMUSPNONYJMTEJHQHQFOOPUPBCYAÑJONCNNQHNMONDHKUTJMQCMOPNFAOXNTNLOAZMJDQYMOZCJRNBAOQTUIENFAIXTLXJGAZMJAXJVAZMUDNMYLNLJMUMUYHVUMHTÑIGDXDQUCLSJPIBCUSFNUGXXGEEXKAEJMESJÑENASLHLBAEYJROJXACQTCNMYPUCUNMJWOYNHZNKUOGAJDUJXENRYTENJSCNMONTYJNMJYFXFIGJMIBUUSNTFAPNFAFKUROJNYCTUYNBYSGJVACAUCGQWAZMJJHJHSNTPAPXMGNECOGJUTENCNGJGEGAJSPNULGDMAÑJDOFDNPUNNPNTGENMJSNTTOFDKIOXSSQNNFBATOCXMMNVÑEZNMEZBOSNTUSQBUDBTJRBBUYPQZIOQFTBANIBVMEDDYRUMUPNAULBOMAEDHVHNFOCJOSNMJ"
-# key_lengths = calc_key_lengths(C1)
-# print(key_lengths)
-# print(shift_with_key("PARAQUELACOSANOME", [0,1,4,18]))
 kasiski_decipher(C1)
--- shift_cipher.py
+++ shift_cipher.py
@@ -6,32 +6,32 @@ ENG_ALPH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 SPA_ALPH = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
 FREQ_PAIRS_SPA = [['A', 0.1253],
-             ['B', 0.0142],
-             ['C', 0.0468],
-             ['D', 0.0586],
-             ['E', 0.1368],
-             ['F', 0.0069],
-             ['G', 0.0101],
-             ['H', 0.0070],
-             ['I', 0.0625],
-             ['J', 0.0044],
-             ['K', 0.0002],
-             ['L', 0.0497],
-             ['M', 0.0315],
-             ['N', 0.0671],
-             ['Ñ', 0.0031],
-             ['O', 0.0868],
-             ['P', 0.0251],
-             ['Q', 0.0088],
-             ['R', 0.0687],
-             ['S', 0.0798],
-             ['T', 0.0463],
-             ['U', 0.0393],
-             ['V', 0.0090],
-             ['W', 0.0001],
-             ['X', 0.0022],
-             ['Y', 0.0090],
-             ['Z', 0.0052]]
+                  ['B', 0.0142],
+                  ['C', 0.0468],
+                  ['D', 0.0586],
+                  ['E', 0.1368],
+                  ['F', 0.0069],
+                  ['G', 0.0101],
+                  ['H', 0.0070],
+                  ['I', 0.0625],
+                  ['J', 0.0044],
+                  ['K', 0.0002],
+                  ['L', 0.0497],
+                  ['M', 0.0315],
+                  ['N', 0.0671],
+                  ['Ñ', 0.0031],
+                  ['O', 0.0868],
+                  ['P', 0.0251],
+                  ['Q', 0.0088],
+                  ['R', 0.0687],
+                  ['S', 0.0798],
+                  ['T', 0.0463],
+                  ['U', 0.0393],
+                  ['V', 0.0090],
+                  ['W', 0.0001],
+                  ['X', 0.0022],
+                  ['Y', 0.0090],
+                  ['Z', 0.0052]]
 FREQS_SPA = [freq_pair[1] for freq_pair in FREQ_PAIRS_SPA]
 I = sum(p**2 for p in FREQS_SPA)
 
@@ -40,37 +40,36 @@ def break_shift_cipher(s):
     p = FREQS_SPA
     q = calc_freqs(s)
     alph_len = len(p)
-    I_list = [sum([p[i]*q[(i + j)%alph_len] for i in range(alph_len)]) 
-                                            for j in range(alph_len)]
-    return I_list.index(min(I_list, key=lambda k:abs(I-k)))
+    I_list = [sum([p[i]*q[(i+j) % alph_len] for i in range(alph_len)])
+              for j in range(alph_len)]
+    return I_list.index(min(I_list, key=lambda k: abs(I-k)))
 
 
 def calc_freqs(s):
-    freq_pairs = [[c,0] for c in SPA_ALPH]
+    freq_pairs = [[c, 0] for c in SPA_ALPH]
     for c in s:
         freq_pairs[char_index(c)][1] += 1
     return [float(freq_pair[1])/len(s) for freq_pair in freq_pairs]
-        
+
 
 def shift(s, offset):
     return ''.join([shift_char(c, offset) for c in s])
 
 
 def shift_char(c, offset):
-    shifted_index = char_index(c) + offset    
-    alph_index = shifted_index%len(SPA_ALPH)
+    shifted_index = char_index(c) + offset
+    alph_index = shifted_index % len(SPA_ALPH)
     return SPA_ALPH[alph_index]
 
 
 def char_index(c):
     n_index = ord('N') - ord('A')
     if c.upper() == 'Ñ':
-        return n_index + 1 
-    index = ord(c.upper()) - ord('A') 
-    return index + (1 if index > n_index else 0) # It's at Ñ's position or past it
-
+        return n_index + 1
+    index = ord(c.upper()) - ord('A')
+    # It's at Ñ's position or past it
+    return index + (1 if index > n_index else 0)
 
 
 code = "VKXYKBKXGKSGWAKQQGYIUYGYWAKXKGQRKSZKJKYKKYIUSYKMAÑX"
 offset = break_shift_cipher(code)
-# print(shift(code, -offset))
